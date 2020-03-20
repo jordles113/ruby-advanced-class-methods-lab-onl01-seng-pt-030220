4586298b15d65214c1bdfe41b960f7f264fb261d
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-20 20:12:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,10 +39,10 @@ class Song
     @@all.sort_by{|x| x.name}
   end 
   
-  def self.new_from_filename(name)
+ def self.new_from_filename(name)
     song = self.new 
-    song.name = (name.split("-")[1].chomp (".mp3"))
-    song.artist_name = (name.split("-")[0])
-    song 
-  end 
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    song
+  end
 end
